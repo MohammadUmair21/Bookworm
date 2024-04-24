2f99fa00909e73750017711bd16ae8f1f39bcd49
UmairAnsari1 <ansariumairislam21@gmail.com>
2024-04-24 15:48:32 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Umair on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
