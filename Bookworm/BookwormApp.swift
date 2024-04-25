dc2b39c22b02b0bd7aa27b701cac22792d063d55
UmairAnsari1 <ansariumairislam21@gmail.com>
2024-04-25 15:24:18 +0530

    Bookworm app source code

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -5,6 +5,7 @@
 //  Created by Umair on 24/04/24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct BookwormApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for : Book.self)
     }
 }
---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Umair on 25/04/24.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    
+    let rating : Int
+    
+    var body: some View {
+        switch rating {
+        case 1 :
+            Text("😴")
+        case 2 :
+            Text("🥱")
+        case 3 :
+            Text("😕")
+        case 4 :
+            Text("😊")
+        default :
+            Text("🤩")
+            
+        }
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 3)
+}
